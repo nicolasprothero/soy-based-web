e63aa5caaa45bef1e0b10a5cd8b4f049abbb4631
nicolasprothero <nprothero0917@gmail.com>
2025-02-11 14:11:56 -0500

    deploy - assetPrefix add

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@
 const nextConfig = {
   output: 'export',
   basePath: "",
+  assetPrefix: ".",
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
