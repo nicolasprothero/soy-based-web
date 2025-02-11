1e2e2126b5156bbf0c62b3f2408e533a87476618
nicolasprothero <nprothero0917@gmail.com>
2025-02-11 12:44:12 -0500

    deployment - rollback to next 14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
-
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -29,7 +18,7 @@ export default function RootLayout({
         <title>{"Soy Based Web"}</title>
         <meta name="description" content="SOY BASED WEB BY SOY BASED RISO" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Header />
         <Sidebar />
         <Footer />
